3bfe5097e79583b79ee2f5413252f26cd4c57952
Rajeshkumar Chandrasekaran <rajeshkumar.chandrasekaran@optus.com.au>
2025-08-09 22:30:59 +1000

    Refactor build.gradle.kts to use version catalog and update dependencies
    (cherry picked from commit c23e8e3c07fc19e3dd74af636d3d0c6e6ed264d8)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
-// Top-level build file (project-level build.gradle.kts)
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.google.dagger.hilt.android") version "2.50" apply false
-    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.google.dagger.hilt.android) apply false
+    alias(libs.plugins.google.devtools.ksp) apply false
 }
